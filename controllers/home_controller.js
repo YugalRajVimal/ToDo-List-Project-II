8cc57a3e7f3ec5b0db5f7a19a0d09e6cb0050580
YugalRajVimal <sagaryugal472@gmail.com>
2023-08-28 00:10:02 +0530

    ToDo List's assets folder and files created and task input structure modified

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,7 @@
 module.exports.home = function(request,response){
-    return response.end('<h1>Express is up for ToDo List</h1>');
+    // return response.end('<h1>Express is up for ToDo List</h1>');
+    return response.render('home',{
+        "title" : "ToDo List",
+        "heading":"My ToDo List"
+    });
 }
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 
 const port = 8000;
 
 //Use Express Router 
 app.use('/',require('./routes'));
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+app.use(express.urlencoded());
+app.use(express.static('assets'));
+
 
 app.listen(port,function(error){
     if(error){
